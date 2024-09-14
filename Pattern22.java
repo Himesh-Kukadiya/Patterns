bdb51780be9b67ed2498b08b2183f3b53040038a
Himesh-Kukadiya <himeshkukadiya075@gmail.com>
2024-09-14 18:20:33 +0530

    Pattern22

---
+++ Pattern22.java
@@ -0,0 +1,18 @@
+// 0  0  0  0  0 
+// 1  1  1  1  1 
+// 0  0  0  0  0 
+// 1  1  1  1  1
+// 0  0  0  0  0
+
+public class Pattern22 {
+    public static void main(String[] args) {
+        int n = 5;
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 1; j <= n; j++) {
+                System.out.print(" " + i % 2 + " ");
+            }
+            System.out.println();
+        }
+    }
+}
