4bc4d618269ea60acfd71867ae099dc4211b7455
Himesh-Kukadiya <himeshkukadiya075@gmail.com>
2024-09-14 18:14:24 +0530

    Pattern20

---
+++ Pattern20.java
@@ -0,0 +1,18 @@
+// 1  0  1  0  1 
+// 0  0  0  0  0 
+// 1  0  1  0  1 
+// 0  0  0  0  0
+// 1  0  1  0  1
+
+public class Pattern20 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                System.out.print(i%2 == 0 ? " " + i%2 + " " : " " + j % 2 + " "); 
+            }
+            System.out.println();
+        }
+    }
+}
