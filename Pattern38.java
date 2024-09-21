07042673fbd3cd2eebe2a05362a7c659f49c5d37
Himesh-Kukadiya <himeshkukadiya075@gmail.com>
2024-09-21 21:15:31 +0530

    Pattern38

---
+++ Pattern38.java
@@ -0,0 +1,18 @@
+// 5 
+// 5  4
+// 5  4  3
+// 5  4  3  2
+// 5  4  3  2  1
+
+public class Pattern38 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for(int i = n; i >= 1; i--) {
+            for(int j = n; j >= i; j--) {
+                System.out.print(" " + j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
